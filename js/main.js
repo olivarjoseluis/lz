148bf27b3c0a3268a5391b0e9e52ef2afa2b5e3f
Jose Luis Olivar <jose.olivar@javerianacali.edu.co>
2020-08-10 14:59:06 -0500

    Login

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//Atributos para slider principal
+jQuery(".custom-slider-news").owlCarousel({
+    loop: true,
+    items: 1,
+    nav: false,
+    dots: true,
+    center: true,
+    autoplay: true,
+    autoplayTimeout: 10000,
+    autoplaySpeed: 1000,
+    responsiveClass: true
+});
